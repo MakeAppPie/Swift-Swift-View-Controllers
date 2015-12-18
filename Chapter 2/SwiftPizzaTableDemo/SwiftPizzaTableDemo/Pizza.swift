24662c34535963e6cb82c367a9c36ac3fa92feb0
Steven Lipton <shlomo626@gmail.com>
2015-12-18 06:56:23 -0600

    Comment additions
    Added MARK: comments to code.

--- Chapter 2/SwiftPizzaTableDemo/SwiftPizzaTableDemo/Pizza.swift
+++ Chapter 2/SwiftPizzaTableDemo/SwiftPizzaTableDemo/Pizza.swift
@@ -5,9 +5,11 @@
 //  Created by Steven Lipton on 9/25/15.
 //  Copyright © 2015 Steven Lipton. All rights reserved.
 //
-
+// github edited file
+// The basic model for exercises in this chapter. 
 import UIKit
 class Pizza {
+//MARK: Properties
     var pizzaPricePerInSq = ["Cheese": 0.03 ,
         "Sausage": 0.06 ,
         "Pepperoni": 0.05 ,
@@ -16,7 +18,7 @@ class Pizza {
     let pi = 3.1415926
     var diameter = 0.0
     var pizzaType = "Cheese"
-    
+ //MARK: Computed properties   
     var radius : Double {
         get{
             return diameter/2.0
@@ -30,7 +32,7 @@ class Pizza {
         }
     }
  
-    
+    //MARK: - Instance Methods 
     func pizzaArea() -> Double{
         return radius * radius * pi
     }
